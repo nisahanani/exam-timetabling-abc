d3b6c2a7412eeb50e4e649e4f60571144d3236b6
nisahanani <nisahanani4@gmail.com>
2026-01-14 12:46:27 +0800

    Update abc_algorithm.py

--- abc_algorithm.py
+++ abc_algorithm.py
@@ -65,3 +65,25 @@ for sol in population:
         convergence_curve.append(best_cost)
 
     return best_solution, best_cost, convergence_curve
+
+def calculate_cost(schedule):
+    capacity_violation = 0
+    wasted_capacity = 0
+
+    for s in schedule:
+        if s["num_students"] > s["capacity"]:
+            capacity_violation += (s["num_students"] - s["capacity"])
+        else:
+            wasted_capacity += (s["capacity"] - s["num_students"])
+
+    # Objectives
+    alpha = 50
+    beta = 5
+    total_cost = alpha * capacity_violation + beta * wasted_capacity
+    return total_cost
+
+
+def calculate_fitness(schedule):
+    cost = calculate_cost(schedule)
+    fitness = 1 / (1 + cost)
+    return fitness, cost
